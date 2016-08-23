9a58a53a5d83161211ce725b364f0b6de5581499
David Raposa <drap128@cox.net>
2016-08-23 20:06:23 +0000

    finish FlickList 1 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
+  token: "2c7ef89dcc36c66bd9b43f80517df222" // TODO 0 put your api key here
 }
 
 
@@ -29,7 +29,7 @@ function discoverMovies(callback) {
 			
 			// TODO 2
 			// update the model, setting its .browseItems property equal to the movies we recieved in the response
-			
+			model.browseItems = response.results;
 			// invoke the callback function that was passed in. 
 			callback();
 		}
@@ -44,20 +44,35 @@ function discoverMovies(callback) {
 function render() {
   // TODO 7
   // clear everything from both lists
+	var watch = $('#section-watchlist').find("ul");
+  $('li').remove();
   
   // TODO 6
   // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
-  
+  model.watchlistItems.forEach(function(movie) {
+				watch = $('#section-watchlist').find("ul");
+				$("<li>" + movie.title + "</li>").insertAfter(watch); 
+				console.log(model.watchlistItems);
+
+  });  
   // for each movie on the current browse list, 
-  model.browseItems.forEach(function(movie) {
+  model.browseItems.forEach(function(movie, index) {
 		// TODO 3
 		// insert a list item into the <ul> in the browse section
-		
+		var ul = $('#section-browselist ul');
+		var title = $('<p></p>').text(movie.original_title);
+		$(ul).append(title); 
 		// TODO 4
 		// the list item should include a button that says "Add to Watchlist"
-		
+		var myButton = $('<button></button>').text("Add to watchlist.");
+		var movieItem = $(ul).append(title).append(myButton);
 		// TODO 5
 		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+		myButton.click(function(){
+			model.watchlistItems.push(movie);
+//			console.log(model.watchlistItems);
+			render();
+		});
   });
   
 }
